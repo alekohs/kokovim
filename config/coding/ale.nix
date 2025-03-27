deb37371f03393f3f3aef9fd7baac3da6eb180dc
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-03-27 19:11:48 +0100

    Moved ale to lua config

--- config/coding/ale.nix
+++ config/coding/ale.nix
@@ -17,23 +17,26 @@
 
   extraConfigLua = # Lua
     ''
-      -- Basic ALE setup
-      vim.g.ale_linters_explicit = 1  -- only use explicit linters
-      vim.g.ale_linters = {
-        ['javascript'] = {'eslint', 'prettier'},
-        ['lua'] = {'luacheck'},
-        ['nix'] = {'dead-nix'},
-      }
+      require("ale").setup({
+        disable_lsp = "auto",
+        sign_info = vim.g.symbol_info,
+        sign_error = vim.g.symbol_error,
+        sign_warning = vim.g.symbol_warn,
+        sign_priority = 4,
+        open_list = 0,
+        echo_msg_format = "%severity% [%linter%] (%code%) - %s",
+        lint_on_text_changed = 'always',
 
-      vim.g.ale_fixers = {
-        ['nix'] = {'nixfmt'},
-      }
+        linters = {
+          javascript = { "eslint", "prettier" },
+          luacheck = { "luacheck" },
+          nix = { "dead-nix" },
+        },
+        fixers = {
+          nix = { "nixfmt-rfc-style" },
+        },
 
-      -- vim.g.ale_lint_on_save = 1  -- Lint on save
-      vim.g.ale_lint_on_text_changed = 'always'  -- Lint on text change
-      -- vim.g.ale_fix_on_save = 1  -- Fix on save
-      vim.g.ale_echo_msg_error_str = 'Error:'
-      vim.g.ale_echo_msg_warning_str = 'Warning:'
+      })
     '';
 
   keymaps = [ ];
