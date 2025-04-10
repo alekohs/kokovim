2d0ecf4a5ec783bf57555790c33844ccf1509f97
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-04-10 15:41:57 +0200

    Removed italics from variables (#52)

--- config/ui/styles.nix
+++ config/ui/styles.nix
@@ -2,6 +2,29 @@
   colorschemes.rose-pine = {
     enable = true;
     lazyLoad.enable = true;
+    settings = {
+      highlight_groups = {
+        "@parameter" = {
+          italic = false;
+        };
+        "@property" = {
+          italic = false;
+        };
+        "@variable" = {
+          italic = false;
+        };
+        "@variable.builtin" = {
+          italic = false;
+        };
+        "@variable.parameter" = {
+          italic = false;
+        };
+        "@variable.parameter.builtin" = {
+          italic = false;
+        };
+      };
+
+    };
   };
 
 }
