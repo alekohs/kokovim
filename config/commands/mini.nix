f87fca1b529322ac8edf5d53db0f7d63d2beff36
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-04-13 20:54:48 +0200

    Added dotnet createfile option to mini files

--- config/commands/mini.nix
+++ config/commands/mini.nix
@@ -11,5 +11,39 @@
       '';
     }
 
+    {
+      event = "User";
+      desc = "Add mini file keymaps";
+      pattern = "MiniFilesBufferCreate";
+      callback.__raw = ''
+        function(args)
+          local buf_id = args.data.buf_id
+
+          local function get_path()
+            local entry = require('mini.files').get_fs_entry()
+            if not entry then
+              return nil
+            end
+
+            if entry.fs_type == 'directory' then
+              return entry.path
+            else
+              return vim.fs.dirname(entry.path) -- Strip to parent directory
+            end
+          end
+
+          vim.keymap.set('n', '<leader>N', function()
+            local path = get_path()
+            if path then
+              require('easy-dotnet').createfile(path)
+            else
+              vim.notify('No folder selected', vim.log.levels.ERROR)
+            end
+          end, { buffer = buf_id, desc = "Create new dotnet file" })
+        end
+      '';
+    }
+
+
   ];
 }
