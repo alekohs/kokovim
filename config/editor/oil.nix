3542a67d318927b3fa5a66ba687c19c1df547c6c
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-04-28 07:37:35 +0200

    Added hidden files for oil

--- config/editor/oil.nix
+++ config/editor/oil.nix
@@ -3,14 +3,27 @@
     enable = true;
   };
 
-
   keymaps = [
-      {
-        mode = "n";
-        action = "<CMD>Oil --float<CR>";
-        key = "<leader>o";
-        options.desc = "Open oil";
-        options.silent = true;
-      }
+    {
+      mode = "n";
+      action = "<CMD>Oil --float<CR>";
+      key = "<leader>o";
+      options.desc = "Open oil";
+      options.silent = true;
+    }
+  ];
+
+  autoCmd = [
+    {
+      event = "FileType";
+      desc = "Toggle hidden files";
+      pattern = "oil";
+      callback.__raw = ''
+      function()
+        local opts = { buffer = true }
+        vim.keymap.set("n", "<S-h>", function() require("oil").toggle_hidden() end, opts)
+      end
+      '';
+    }
   ];
 }
