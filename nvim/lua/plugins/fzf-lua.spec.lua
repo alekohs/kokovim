3ce454055d64cacb0e7354958d0b79db7f4d5273
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-05-19 19:26:30 +0200

    fix: lsp settings

--- nvim/lua/plugins/fzf-lua.spec.lua
+++ nvim/lua/plugins/fzf-lua.spec.lua
@@ -11,10 +11,13 @@ return kokovim.get_plugin_by_repo("ibhagwan/fzf-lua", {
     { "<leader>fg", function() require("fzf-lua").git_files() end, desc = "Find git files" },
     { "<leader>fr", function() require("fzf-lua").oldfiles() end, desc = "Find recent files" },
 
+    { "<leader>ssd", function() require("fzf-lua").lsp_document_symbols() end, desc = "Search document symbols" },
+    { "<leader>ssw", function() require("fzf-lua").lsp_workspace_symbols() end, desc = "Search workspace symbols" },
     { "<leader>sR", function() require("fzf-lua").registers() end, desc = "Search registers" },
     { "<leader>sh", function() require("fzf-lua").helptags() end, desc = "Search help tags" },
     { "<leader>sm", function() require("fzf-lua").marks() end, desc = "Search marks" },
     { "<leader>sM", function() require("fzf-lua").man_pages() end, desc = "Search man pages" },
+    { "<leader>sT", function() require("fzf-lua").tmux() end, desc = "Search tmux buffers" },
     { "<leader>sq", function() require("fzf-lua").quickfix() end, desc = "Search quickfix list" },
   },
 })
