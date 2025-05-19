f511092e44f5e13ffc466710352192b84bd40cf8
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-05-19 20:23:49 +0200

    fix: dashboard

--- nvim/lua/plugins/dashboard.spec.lua
+++ nvim/lua/plugins/dashboard.spec.lua
@@ -7,6 +7,31 @@ return {
         week_header = {
           enable = true,
         },
+        project = {
+          action = function(path) require("fzf-lua").files({ cwd = path }) end
+        },
+        shortcut = {
+          {
+            icon = " ",
+            icon_hl = "@variable",
+            desc = "Files",
+            group = "Label",
+            action = function() require("fzf-lua").files() end,
+            key = "f",
+          },
+          {
+            desc = " LazyGit",
+            group = "DiagnosticHint",
+            action = "LazyGit",
+            key = "a",
+          },
+          {
+            desc = " dotfiles",
+            group = "Number",
+            action = function() require("fzf-lua").files({ cwd = "~/.config" }) end,
+            key = "d",
+          },
+        },
       },
     },
     config = function(_, opts) require("dashboard").setup(opts) end,
