f6864cf5329f12fda3684b10f1c4237a3fc3bcb8
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-05-22 11:25:05 +0200

    add: nvim-dap

--- nix/packages.nix
+++ nix/packages.nix
@@ -72,6 +72,8 @@ in
       shellcheck
       sqlfluff
 
+      # DAP
+      netcoredbg
     ]
     ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
       pkgs.fswatch # https://github.com/neovim/neovim/pull/27347
---
+++ nvim/lua/plugins/dap/ui.lua
@@ -0,0 +1,27 @@
+local go = require("plugins.dap.go")
+local python = require("plugins.dap.python")
+
+return kokovim.get_plugin_by_repo("rcarriga/nvim-dap-ui", {
+  opts = {},
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
+  },
+  config = function(_, opts)
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup(opts)
+
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open({}) end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close({}) end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close({}) end
+  end,
+  dependencies = {
+    kokovim.get_plugin_by_repo("nvim-neotest/nvim-nio"),
+    kokovim.get_plugin_by_repo("theHamsta/nvim-dap-virtual-text", {
+      opts = {},
+    }),
+    go,
+    python,
+  },
+})
