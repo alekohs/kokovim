25d4741198c6745dbb3db3a47a5d22a6b1a16b8e
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-05-23 13:23:11 +0200

    bug: fix bin & obj folder in non-project parents (#71)

--- nvim/lua/plugins/lsp/dotnet.lua
+++ nvim/lua/plugins/lsp/dotnet.lua
@@ -9,15 +9,10 @@ local cmd = kokovim.is_nix
       "--logLevel=Information",
       "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
       "--stdio",
-      "--razorSourceGenerator="
-        .. vim.fs.joinpath(remove_bin_suffix(paths.rzls), "lib", "rzls", "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-      "--razorDesignTimePath=" .. vim.fs.joinpath(
-        remove_bin_suffix(paths.rzls),
-        "lib",
-        "rzls",
-        "Targets",
-        "Microsoft.NET.Sdk.Razor.DesignTime.targets"
-      ),
+      -- TODO: This code makes bin/ obj/ folders in non-project folders. Assembly info duplicates
+      -- "--razorSourceGenerator=" .. vim.fs.joinpath(remove_bin_suffix(paths.rzls), "lib", "rzls", "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+      -- "--razorDesignTimePath="
+      --   .. vim.fs.joinpath(remove_bin_suffix(paths.rzls), "lib", "rzls", "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
     }
   or {
     "dotnet",
@@ -28,6 +23,51 @@ local cmd = kokovim.is_nix
     "--razorSourceGenerator=" .. vim.fs.joinpath(paths.roslyn, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
     "--razorDesignTimePath=" .. vim.fs.joinpath(paths.rzls, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
   }
+
+---@class LineRange
+---@field line integer
+---@field character integer
+
+---@class EditRange
+---@field start LineRange
+---@field end LineRange
+
+---@class TextEdit
+---@field newText string
+---@field range EditRange
+
+---@param edit TextEdit
+local function apply_vs_text_edit(edit)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local start_line = edit.range.start.line
+  local start_char = edit.range.start.character
+  local end_line = edit.range["end"].line
+  local end_char = edit.range["end"].character
+
+  local newText = string.gsub(edit.newText, "\r", "")
+  local lines = vim.split(newText, "\n")
+
+  local placeholder_row = -1
+  local placeholder_col = -1
+
+  -- placeholder handling
+  for i, line in ipairs(lines) do
+    local pos = string.find(line, "%$0")
+    if pos then
+      lines[i] = string.gsub(line, "%$0", "", 1)
+      placeholder_row = start_line + i - 1
+      placeholder_col = pos - 1
+      break
+    end
+  end
+
+  vim.api.nvim_buf_set_text(bufnr, start_line, start_char, end_line, end_char, lines)
+
+  if placeholder_row ~= -1 and placeholder_col ~= -1 then
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_cursor(win, { placeholder_row + 1, placeholder_col })
+  end
+end
 
 return {
   kokovim.get_plugin_by_repo("seblyng/roslyn.nvim", {
@@ -48,14 +88,24 @@ return {
           },
           -- picker = "snacks"
         },
-        config = function(_, opts) require("easy-dotnet").setup(opts) end,
       }),
     },
     opts = {
       broad_search = true,
       config = {
         cmd = cmd,
-        handlers = require("rzls.roslyn_handlers"),
+        capabilities = {
+          textDocument = {
+            _vs_onAutoInsert = { dynamicRegistration = false },
+          },
+        },
+        handlers = {
+          -- require("rzls.roslyn_handlers"),
+          ["textDocument/_vs_onAutoInsert"] = function(err, result, _)
+            if err or not result then return end
+            apply_vs_text_edit(result._vs_textEdit)
+          end,
+        },
         settings = {
           ["csharp|completion"] = {
             dotnet_provide_regex_completions = true,
