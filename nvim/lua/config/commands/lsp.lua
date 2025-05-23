7c3b747e7a1e691e990044b57973ff0ee0ea13b7
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-05-23 13:22:57 +0200

    bug: fix bin & obj folder in non-project parents

--- nvim/lua/config/commands/lsp.lua
+++ nvim/lua/config/commands/lsp.lua
@@ -7,3 +7,27 @@ autocmd("FileType", {
   pattern = { "cs", "razor" },
   command = "setlocal tabstop=4 shiftwidth=4",
 })
+
+autocmd("InsertCharPre", {
+  pattern = "*.cs",
+  callback = function()
+    local char = vim.v.char
+
+    if char ~= "/" then return end
+
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    row, col = row - 1, col + 1
+    local bufnr = vim.api.nvim_get_current_buf()
+    local uri = vim.uri_from_bufnr(bufnr)
+
+    local params = {
+      _vs_textDocument = { uri = uri },
+      _vs_position = { line = row, character = col },
+      _vs_ch = char,
+      _vs_options = { tabSize = 4, insertSpaces = true },
+    }
+
+    -- NOTE: we should send textDocument/_vs_onAutoInsert request only after buffer has changed.
+    vim.defer_fn(function() vim.lsp.buf_request(bufnr, "textDocument/_vs_onAutoInsert", params) end, 1)
+  end,
+})
