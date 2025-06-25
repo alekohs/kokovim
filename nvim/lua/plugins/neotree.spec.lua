17da8e4ff9d84585df422718c2f864dc527e4ea2
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-06-25 09:05:11 +0200

    fix: bufferline keymaps (#94)

--- nvim/lua/plugins/neotree.spec.lua
+++ nvim/lua/plugins/neotree.spec.lua
@@ -21,11 +21,6 @@ return {
       },
       { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
       { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-      {
-        "<leader>be",
-        function() require("neo-tree.command").execute({ source = "buffers", toggle = true }) end,
-        desc = "Buffer Explorer",
-      },
     },
     deactivate = function() vim.cmd([[Neotree close]]) end,
     init = function()
