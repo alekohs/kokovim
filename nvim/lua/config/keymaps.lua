217d5caadcddabcff71a3639a5e289bfdfedbd9f
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-08-28 10:19:58 +0200

    bug: use vim registers and specify when to use clipboard (#116)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,14 +1,28 @@
+-- Key mappings for system clipboard
+local mappings = {
+  { mode = "n", keys = { "y", "Y", "p", "P" } },
+  { mode = "v", keys = { "y", "p" } },
+}
+
+for _, map in ipairs(mappings) do
+  for _, key in ipairs(map.keys) do
+    local rhs = '"+' .. key
+    vim.keymap.set(map.mode, "<A-" .. key .. ">", rhs, { noremap = true, silent = true })
+  end
+end
+
 -- Tabs
 vim.keymap.set("n", "<leader><tab>o", "<CMD>tabonly<CR>", { desc = "Close Other tabs" })
 vim.keymap.set("n", "<leader><tab>l", "<CMD>tablast<CR>", { desc = "Last tab" })
 vim.keymap.set("n", "<leader><tab>f", "<CMD>tabfirst<CR>", { desc = "First tab" })
 vim.keymap.set("n", "<leader><tab><tab>", "<CMD>tabnew<CR>", { desc = "New Tab" })
-vim.keymap.set("n", "<leader><tab>]", "<CMD>tabnext<CR>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader><tab>d", "<CMD>tabclose<CR>", { desc = "Close Tab" })
+vim.keymap.set("n", "<leader><tab>]", "<CMD>tabnext<CR>", { desc = "Next Tab" })
+vim.keymap.set("n", "<tab>]", "<CMD>tabnext<CR>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader><tab>[", "<CMD>tabprevious<CR>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<tab>[", "<CMD>tabprevious<CR>", { desc = "Previous Tab" })
 
 -- Windows
 vim.keymap.set("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
 vim.keymap.set("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
 vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-
