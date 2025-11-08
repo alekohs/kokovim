6623a6fd22936cae254ad92e39c169ce38eaf58d
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-11-08 07:18:58 +0100

    add: treesitter-textobjects

--- nvim/lua/plugins/treesitter.spec.lua
+++ nvim/lua/plugins/treesitter.spec.lua
@@ -6,7 +6,7 @@ local disabled_files = {
 }
 
 local disabled_filetypes = {
-  "dockerfile",
+  -- "dockerfile",
   -- "sh",
   "tmux",
 }
@@ -75,7 +75,6 @@ return {
     event = { "BufReadPost", "BufNewFile", "VeryLazy" },
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     dependencies = {
-      kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter-refactor"),
       kokovim.get_plugin_by_repo("windwp/nvim-ts-autotag", { lazy = false }),
     },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
@@ -142,22 +141,100 @@ return {
   }),
   kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter-context", {
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter"),
+    },
     opts = function()
       local tsc = require("treesitter-context")
       vim.keymap.set("n", "<leader>ut", function() tsc.enable() end, { desc = "Toggle tree sitter context" })
-
-      -- Snacks.toggle({
-      --   name = "Treesitter Context",
-      --   get = tsc.enabled,
-      --   set = function(state)
-      --     if state then
-      --       tsc.enable()
-      --     else
-      --       tsc.disable()
-      --     end
-      --   end,
-      -- }):map("<leader>ut")
       return { mode = "cursor", max_lines = 3 }
+    end,
+  }),
+  kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter-refactor", {
+    dependencies = {
+      kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter"),
+    },
+  }),
+  kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter-textobjects", {
+    dependencies = {
+      kokovim.get_plugin_by_repo("nvim-treesitter/nvim-treesitter"),
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = { query = "@function.outer", desc = "Next function start" },
+              ["]]"] = { query = "@class.outer", desc = "Next class start" },
+
+              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+              ["]o"] = "@loop.*",
+              ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+              ["]M"] = { query = "@function.outer", desc = "Go to next function end" },
+              ["]["] = { query = "@class.outer", desc = "Go to next class end" },
+            },
+            goto_previous_start = {
+              ["[f"] = { query = "@function.outer", desc = "Go to previous function start" },
+              ["[["] = { query = "@class.outer", desc = "Go to previous class start" },
+            },
+            goto_previous_end = {
+              ["[F"] = { query = "@function.outer", desc = "Go to previous function end" },
+              ["[]"] = { query = "@class.outer", desc = "Go to previous class end" },
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+              ["]d"] = { query = "@conditional.outer", desc = "" },
+            },
+            goto_previous = {
+              ["[d"] = { query = "@conditional.outer", desc = "" },
+            },
+          },
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+              ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+              ["ac"] = { query = "@class.outer", desc = "Selecter outer part of a class" },
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
+          },
+        },
+      })
     end,
   }),
 }
