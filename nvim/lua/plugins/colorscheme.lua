3ef4ae4fa94da58cedd1dd023f913c90a2d6fb14
Alexander Kohkoinen Morin <alexander@alekoh.se>
2026-03-27 07:45:29 +0100

    fix: black bg for rose-pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,19 @@ return {
       dark_variant = "main",
       dim_inactive_windows = false,
       extend_background_behind_borders = true,
+      palette = {
+        main = {
+          base = "#141416",
+          surface = "#242426",
+          overlay = "#3e3e42",
+          muted = "#908f92",
+          subtle = "#6e6e72",
+          text = "#e0e0e4",
+          highlight_low = "#1c1c1e",
+          highlight_med = "#3e3e42",
+          highlight_high = "#504f54",
+        },
+      },
       styles = {
         transparency = true,
       },
